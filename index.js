d64016469a90ef53507abb536bdf492957dea8c1
arfost <clement.chevalier@cirruseo.com>
2017-11-14 14:51:05 +0100

    better config system

--- index.js
+++ index.js
@@ -5,105 +5,110 @@ var jsonWriter = new Map();
 
 var tmpJsonWrite = require('./defaultWriter.json');
 
-var addToJsonWriterPool = function(json){
-  for(var writer of json){
+var addToJsonWriterPool = function (json) {
+  for (var writer of json) {
     jsonWriter.set(writer.name, writer.conf);
   }
 }
 
 addToJsonWriterPool(tmpJsonWrite);
 
-var getNewLogger = function(name, confFile, confName){
+var getNewLogger = function (name, confFile, confName) {
   return new Logger(name, confFile, confName, writerPool);
 }
 
-module.exports.getNewLogger = getNewLogger;
+var defaultConfFile = 'node_modules/stealthlog/default.json';
 
-var defaultConfFile = './default.json';
+var getConfiguredInstance = function (confFile) {
+  if (confFile === undefined || confFile === null || confFile == '') {
+    confFile = defaultConfFile;
+  }
+  try {
+    confFile = require(process.cwd() + "/" + confFile);
+  } catch (err) {
+    console.log("Unable to open conf file, using default one", err);
+    confFile = require("./default.json");
+  }
+  if (confFile.customWriters != undefined) {
+    addToJsonWriterPool(confFile.customWriters);
+  }
+  return function (name, confName) {
+    return getNewLogger(name, confFile, confName)
+  }
+}
 
-class Logger{
+module.exports = getConfiguredInstance;
 
-  constructor(name, confFile, confName, writerPool){
+class Logger {
+
+  constructor(name, confFile, confName, writerPool) {
 
     this.name = name;
     this.writerPool = writerPool;
 
-    if (confFile === undefined || confFile === null || confFile == ''){
-      confFile = defaultConfFile;
-    }
-    var confFile;
-    try{
-      confFile = require(confFile);
-    }catch(err){
-      console.log("Unable to open conf file, using default one", err);
-      confFile = require(defaultConfFile);
-    }
-    if(confFile.customWriters != undefined){
-      addToJsonWriterPool(confFile.customWriters);
-    }
-    if(confName != undefined){
+    if (confName != undefined) {
       this.conf = confFile[confName];
-      if(this.conf == undefined)
+      if (this.conf == undefined)
         this.conf = confFile[Object.keys(confFile)[0]];
-    }else{
+    } else {
       this.conf = confFile[Object.keys(confFile)[0]];
     }
   }
 
-  log(){
-    if(this.conf.level.indexOf('LOG') != -1 && this.conf.blackListName.indexOf(this.name) === -1){
+  log() {
+    if (this.conf.level.indexOf('LOG') != -1 && this.conf.blackListName.indexOf(this.name) === -1) {
       this.logToWriters(arguments, 'LOG');
     }
   }
 
-  warn(){
-    if(this.conf.level.indexOf('WARN')  != -1 && this.conf.blackListName.indexOf(this.name) === -1){
+  warn() {
+    if (this.conf.level.indexOf('WARN') != -1 && this.conf.blackListName.indexOf(this.name) === -1) {
       this.logToWriters(arguments, 'WARN');
     }
   }
 
-  error(){
-    if(this.conf.level.indexOf('ERROR')  != -1 && this.conf.blackListName.indexOf(this.name) === -1){
+  error() {
+    if (this.conf.level.indexOf('ERROR') != -1 && this.conf.blackListName.indexOf(this.name) === -1) {
       this.logToWriters(arguments, 'ERROR');
     }
   }
 
-  info(){
-      if(this.conf.level.indexOf('INFO')  != -1 && this.conf.blackListName.indexOf(this.name) === -1){
-        this.logToWriters(arguments, 'INFO');
-      }
+  info() {
+    if (this.conf.level.indexOf('INFO') != -1 && this.conf.blackListName.indexOf(this.name) === -1) {
+      this.logToWriters(arguments, 'INFO');
+    }
   }
 
-  logToWriters(toLog, lvl){
+  logToWriters(toLog, lvl) {
     var intro = {};
     intro.name = this.name;
-    if(this.conf.logTime == true){
+    if (this.conf.logTime == true) {
       intro.time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
     }
-    if(this.conf.logLvl == true){
-      intro.level =  "[" + lvl + "]";
+    if (this.conf.logLvl == true) {
+      intro.level = "[" + lvl + "]";
     }
-    for(var writer of this.conf.writers){
-        var realWriter = this.writerPool.get(writer);
-        //console.log(writer, realWriter, writerPool);
-        if(realWriter === undefined){
-            realWriter = getNewWriter(writer);
-            this.writerPool.set(writer, realWriter);
-        }
-        realWriter.write(intro, toLog);
+    for (var writer of this.conf.writers) {
+      var realWriter = this.writerPool.get(writer);
+      //console.log(writer, realWriter, writerPool);
+      if (realWriter === undefined) {
+        realWriter = getNewWriter(writer);
+        this.writerPool.set(writer, realWriter);
+      }
+      realWriter.write(intro, toLog);
     }
   }
 }
 
-var getNewWriter = function(name){
+var getNewWriter = function (name) {
   //console.log(jsonWriter, name, jsonWriter.get(name));
-    return new Writer(jsonWriter.get(name));
+  return new Writer(jsonWriter.get(name));
 }
 
 var typeLoggerLoader = require('./typeLogger/typeLoggerLoader.js');
 
-class Writer{
-  constructor(conf){
+class Writer {
+  constructor(conf) {
 
     this.writeMethod = typeLoggerLoader('writeType', conf.writeMethod.type);
     this.formatter = typeLoggerLoader('formatter', conf.formatter.type);
@@ -113,12 +118,12 @@ class Writer{
 
   }
 
-  write(intro, toLog){
+  write(intro, toLog) {
     //console.log('writer args send : \n', intro, toLog)
     this.writeMethod.write(this.writeConf, this.format(intro, toLog));
   }
 
-  format(intro, toFormat){
+  format(intro, toFormat) {
     //console.log('formatter args send : \n', this.formatterConf);
     return this.formatter.format(this.formatterConf, intro, toFormat);
   }
--- test.js
+++ test.js
@@ -1,19 +1,14 @@
 'use strict'
 
-var test = require('./index.js');
-var test2 = require('./index.js');
+var test = require('./index.js')();
 
 //console.log(test);
-var logger = test.getNewLogger('T1L1');
-var logger2 = test.getNewLogger('T1L2');
+var logger = test('T1L1');
+var logger2 = test('T1L2');
 
-var logger3 = test2.getNewLogger('T2L1');
-var logger4 = test2.getNewLogger('T2L2');
 
 logger2.log('something something');
 logger.warn('something else, with an object', {'name':'i\'m an object', 'prop':34});
 logger.log('something something');
 logger2.log('something else, with an object', {'name':'i\'m an object', 'prop':34}, 'and a phrase');
 
-logger3.log('something else, with an object', logger2);
-logger4.error('something else, with an object', {'name':'i\'m an object', 'prop':34});
